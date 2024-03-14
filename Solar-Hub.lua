c2a62dc783a9a1a0460b712ac01850655a365eb1
Big Joe <106391483+IrishGypsyUser@users.noreply.github.com>
2024-03-14 21:22:48 +0000

    Update Solar-Hub.lua

--- Solar-Hub.lua
+++ Solar-Hub.lua
@@ -1,9 +1,5 @@
-local HttpGet;
-HttpGet = hookfunction(game.HttpGet,function(self,url,...)
-    if url == "https://raw.githubusercontent.com/xurel7/whitelist/main/whitelisted.lua" then
-        return {[game.Players.LocalPlayer.UserId] = "admin"}
-    end
-    return HttpGet(self,url,...)
-end)
+local whitelisted = {
+   [game.Players.LocalPlayer.UserId] = "admin"
+}
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xurel7/solarhub/main/key-system/loader.lua"))()
+return whitelisted
